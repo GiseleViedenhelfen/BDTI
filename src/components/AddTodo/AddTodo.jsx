1581980138a2181605207c2309595291c1abfa7a
Gisele <gisele.viedenhelfen@gmail.com>
2023-02-19 14:11:37 -0300

    altera o addTodo para componente de classe também

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,39 +1,58 @@
-import React,{ useState } from "react";
+import React,{ Component } from "react";
 import { connect } from "react-redux";
 import { addTodo } from "../../Redux/actions";
 
-const AddTodo = ({ addTodo }) => {
-  // const { addTodo } = this.props;
-  const [newTodo, setNewTodo] = useState('');
-  const [inputValue, setInputValue] = useState('');
-
-  const handleChange = ({target}) => {
-    setInputValue(target.value);
-    setNewTodo(target.value.trim())
-  
+class AddTodo extends Component {
+  constructor() {
+    super();
+    // inicia os valores zerados
+    this.state = {
+      task: '',
+      inputValue: ''
+    };
   }
 
-  const handleClick = () => {
-    addTodo(newTodo);
-    setInputValue('');
-  };
-return (
-  <form>
-    <label htmlFor="todo-input">
-      Tarefa:
-      <input
-        type="text"
-        id="todo-input"
-        value={inputValue}
-        onChange={ handleChange }
-      />
-    </label>
+  handleChange = ({target}) => {
+    //pega o valor do input
+    const newTask =  target.value
+    //salva no estado o valor do input e a nova task a ser colocada no estado global
+    this.setState({
+      inputValue: newTask,
+      // adiciona o trim para nao salvar possiveis espacos vazios
+      task: newTask.trim(),
+    })
+  }
+  handleClick = () => {
+    //pega o valor da task a ser salva
+    const {task} = this.state;
+    // pega a action via props
+    const { addTodo } = this.props;
+    // adiciona o valor da task no estado global
+    addTodo(task);
+    // uma vez salvo no estado global, limpa o input de texto
+    this.setState({inputValue: ''})
+  }
 
-    <button type="button" onClick={handleClick}>
-      Adicionar
-    </button>
+  render() {
+    const { inputValue } = this.state;
+    return(
+    <form>
+      <label htmlFor="todo-input">
+        Tarefa:
+        <input
+          type="text"
+          id="todo-input"
+          value={ inputValue }
+          onChange={ this.handleChange }
+        />
+      </label>
+
+      <button type="button" onClick={this.handleClick}>
+        Adicionar
+      </button>
   </form>
-)
+    )
+  }
 }
 const mapDispatchToProps = (dispatch) => {
   return {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,8 @@ import React,{ Component } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       showAddTodo: false
     };
